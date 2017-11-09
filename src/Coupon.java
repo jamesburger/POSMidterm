245a78ef6ed714909decea5b9228a92a70222455
James Burger <helpingofburgers@gmail.com>
2017-11-09 17:46:38 -0500

    Added coupon class

---
+++ src/Coupon.java
@@ -0,0 +1,12 @@
+
+public class Coupon extends Product{
+	
+	String itemSelect;
+	int dollarAmount = 0; 
+	
+	itemSelect = (shopCart.getProductName((int) Math.random() * shopCart.length())); 
+	dollarAmount = (int) Math.random() * ((shopCart.getProductPrice.getProductName(item)) * 0.2);
+	
+	
+}
+}
